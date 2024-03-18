ee517338e3841849ca4453666555844882e49294
RedKeyD <redkeydead@gmail.com>
2024-03-18 22:41:51 +0300

    added lectures

---
+++ lecture1(14.03)/casino/Casino/Program.cs
@@ -0,0 +1,61 @@
+﻿int[] winValues = new int[ 3 ];
+winValues[ 0 ] = 18;
+winValues[ 1 ] = 19;
+winValues[ 2 ] = 20;
+
+Random random = new Random();
+
+double currentSum = 10000;
+double winSum = 0;
+int bet = 0;
+int randomNum = random.Next( 1, 20 );
+
+string betPrompt = "Введите вашу ставку: ";
+Console.Write( betPrompt );
+
+
+string betInput = Console.ReadLine();
+
+if ( int.TryParse( betInput, out int parsedBet ) )
+{
+    bet = parsedBet;
+}
+else
+{
+    string inputError = "Ошибка. Попробуйте  еще раз ввести ваше значение";
+    Console.WriteLine( inputError );
+    
+}
+if ( currentSum > 0 && bet <= currentSum )
+{
+    string randomNumMessage = $"Вам выпало число {randomNum}";
+    Console.WriteLine( randomNumMessage );
+
+    if ( winValues.Contains( randomNum ) )
+    {
+        winSum = bet * 1 + ( 0.1 * randomNum ) % 17;
+
+        string winMessage = $"Поздравляем с победой вы выиграли сумму в размере {winSum}";
+        Console.WriteLine( winMessage );
+    }
+    else
+    {
+        currentSum -= bet;
+
+        string loseMessage = $"Вы проиграли вашу ставку в размере {bet}";
+        Console.WriteLine( loseMessage );
+        
+    }
+}
+else
+{
+    string insufficientFundsMessage = $"Недостаточно средств на балансе {currentSum}";
+    Console.WriteLine( insufficientFundsMessage );
+}
+
+
+
+
+
+
+
---
+++ lecture1(14.03)/test/Market/Program.cs
@@ -0,0 +1,82 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string productPrompt = "Введите название продукта: ";
+        string countPrompt = "Введите количество продукта: ";
+        string userPrompt = "Введите ваше имя:  ";
+        string addressPrompt = "Введите адресс доставки: ";
+
+        bool isComplete = false;
+
+        while ( !isComplete )
+        {
+            string productName = GetUserInput( productPrompt );
+            int productCount = GetIntegerInput( countPrompt );
+            string userName = GetUserInput( userPrompt );
+            string userAddress = GetUserInput( addressPrompt );
+
+            Console.WriteLine( ConfirmOrder( userName, productCount, productName, userAddress ) );
+            string answer = Console.ReadLine();
+
+            if ( string.IsNullOrWhiteSpace( answer ) )
+                isComplete = true;
+        }
+
+        Console.WriteLine( "Заказ был успешно оформлен!" );
+    }
+
+    static string GetUserInput( string prompt )
+    {
+        string input = null;
+        bool validInput = false;
+
+        do
+        {
+            Console.WriteLine( prompt );
+            try
+            {
+                input = Console.ReadLine();
+                validInput = !string.IsNullOrWhiteSpace( input );
+                if ( !validInput )
+                    throw new Exception( "Данные не могут быть пустыми." );
+            }
+            catch ( Exception ex )
+            {
+                Console.WriteLine( $"Ошибка: {ex.Message}" );
+            }
+        } while ( !validInput );
+
+        return input;
+    }
+
+    static int GetIntegerInput( string prompt )
+    {
+        int input = 0;
+        bool validInput = false;
+
+        do
+        {
+            Console.WriteLine( prompt );
+            try
+            {
+                input = int.Parse( Console.ReadLine() );
+                validInput = true;
+            }
+            catch ( FormatException )
+            {
+                Console.WriteLine( "Ошибка вывода данных" );
+            }
+            catch ( Exception ex )
+            {
+                Console.WriteLine( $"Ошибка: {ex.Message}" );
+            }
+        } while ( !validInput );
+
+        return input;
+    }
+    static string ConfirmOrder( string name, int count, string nameProduct, string adress )
+    {
+        return $"Здраствуйте, {name} вы заказали {count} {nameProduct} на адресс {adress}, все верно?";
+    }
+}
